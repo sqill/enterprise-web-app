4cdbf050680fc742b65608a879396b23039d9508
André Perdigão <andrepcg@gmail.com>
2022-06-07 15:56:00 +0100

    Add more animation stuff

---
+++ components/Checkbox.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import cx from "classnames";
+
+export default function CheckboxComponent({ field, form: { touched, errors }, title, IconClass, ...props }) {
+  const hasError = touched[field.name] && errors[field.name];
+  const labelClassName = cx(
+    "text-sm font-medium text-gray-900 block mb-2",
+    { "text-red-700": hasError }
+  );
+  const inputClassName = cx(
+    "w-4 h-4 border border-gray-300 rounded  focus:ring-3 focus:ring-blue-300 ",
+    { "bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500": hasError }
+  );
+  return  (
+    <div className="flex items-start mb-6">
+      <div className="flex items-center h-5">
+        <input id={field.name} type="checkbox" className={inputClassName} {...field} {...props} />
+      </div>
+      <label htmlFor={field.name} className="ml-2 text-sm font-medium text-gray-900">{title}</label>
+
+      {/* {hasError && <p className="mt-2 text-sm text-red-600">{errors[field.name]}</p>} */}
+    </div>
+  );
+}
--- components/Dashboard/Assets/CreateForm.js
+++ components/Dashboard/Assets/CreateForm.js
@@ -3,16 +3,25 @@ import { Formik, Field, Form, ErrorMessage } from 'formik';
 import MdcWeb from '@meronex/icons/mdc/MdcWeb';
 
 import CustomInputComponent from '../../Input'
+import CustomCheckboxComponent from '../../Checkbox'
 import CustomSelectComponent from '../../Select'
 
 import { createVideoAsset } from '../../../api'
 
-function validateForm({ name, category, asset, asset_type, assets, fps }) {
+function validateForm({ name, category, asset, asset_type, assets, fps, uploadTilesheet, rows, columns, size, count }) {
   const errors = {};
   if (!category) errors.category = 'Required'
   if (!asset && asset_type === "image") errors.asset = 'Required'
   if (!assets && asset_type === "animation") errors.assets = 'Required'
   if (!fps && asset_type === "animation") errors.fps = 'Required'
+
+  if(asset_type === "animation" && uploadTilesheet) {
+    if (!rows) errors.rows = 'Required'
+    if (!columns) errors.columns = 'Required'
+    if (!size) errors.size = 'Required'
+    if (!asset) errors.asset = 'Required'
+    if (!count) errors.count = 'Required'
+  }
 
   return errors;
 }
@@ -28,7 +37,6 @@ const ASSET_TYPES = [
 ]
 
 export default function CreateForm({ create, onSuccess }) {
-
   async function handleFormSubmit(values, { setSubmitting, setStatus }) {
     setSubmitting(true)
     const res = await create({ video_asset: { ...values } })
@@ -41,7 +49,20 @@ export default function CreateForm({ create, onSuccess }) {
 
   return (
     <Formik
-      initialValues={{ name: '', category: 'foreground', asset: '', asset_type: 'image', assets: [], fps: null }}
+      initialValues={{
+        name: '',
+        category: 'foreground',
+        asset: '',
+        asset_type: 'image',
+        assets: [],
+        fps: null,
+        count: null,
+        size: null,
+        columns: null,
+        rows: null,
+        loop: false,
+        uploadTilesheet: false
+      }}
       validate={validateForm}
       onSubmit={handleFormSubmit}
     >
@@ -65,14 +86,78 @@ export default function CreateForm({ create, onSuccess }) {
             />
           </div>
           {values.asset_type === "animation" && (
-            <div className="mb-6">
-              <Field
-                name="fps"
-                component={CustomInputComponent}
-                placeholder="Animation FPS (30)"
-                required={true}
-              />
-            </div>
+            <>
+              <div className="mb-6">
+                <Field
+                  name="uploadTilesheet"
+                  component={CustomCheckboxComponent}
+                  title="Upload prebuilt tilesheet?"
+                />
+              </div>
+              <div className="mb-6">
+                <Field
+                  name="fps"
+                  component={CustomInputComponent}
+                  placeholder="Animation FPS"
+                  required={true}
+                  type="number"
+                  min="1"
+                />
+              </div>
+              <div className="mb-6">
+                <Field
+                  name="loop"
+                  component={CustomCheckboxComponent}
+                  title="Loops?"
+                />
+              </div>
+            </>
+          )}
+          {values["uploadTilesheet"] && (
+            <>
+              <div className="mb-6">
+                <Field
+                  name="columns"
+                  component={CustomInputComponent}
+                  placeholder="Columns (5)"
+                  required={true}
+                  type="number"
+                  min="1"
+                />
+              </div>
+              <div className="mb-6">
+                <Field
+                  name="rows"
+                  component={CustomInputComponent}
+                  placeholder="Rows (5)"
+                  required={true}
+                  type="number"
+                  min="1"
+                />
+              </div>
+              <div className="mb-6">
+                <Field
+                  name="size"
+                  component={CustomInputComponent}
+                  placeholder="Tile size, pixels (128)"
+                  required={true}
+                  type="number"
+                  min="32"
+                  max="512"
+                />
+              </div>
+              <div className="mb-6">
+                <Field
+                  name="count"
+                  component={CustomInputComponent}
+                  placeholder="Number of tiles in tilesheet"
+                  required={true}
+                  type="number"
+                  min="1"
+                  max="30"
+                />
+              </div>
+            </>
           )}
           {values.asset_type === "image" && (
             <div className="mb-6">
@@ -93,7 +178,7 @@ export default function CreateForm({ create, onSuccess }) {
               accept="image/*"
             /> */}
             <Field name="image">
-              {() => values.asset_type === "image" ? (
+              {() => values.asset_type === "image" || values["uploadTilesheet"] ? (
                 <input name="asset" accept="image/*" type="file" onChange={e => setFieldValue("asset", e.target.files[0])} />
               ) : (
                 <input name="asset" accept="image/*" type="file" multiple onChange={e => setFieldValue("assets", e.target.files)} />
