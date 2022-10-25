37c3f5370f4930b31c3f35b9eb804b29bb3150d3
Andre Perdigao <andrepcg@gmail.com>
2022-10-25 11:17:43 +0100

    Add sentry

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 // next.config.js
-module.exports = {
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const moduleExports = {
   images: {
     domains: ['sqill.s3.eu-west-3.amazonaws.com', 'heysqill.com', 'sqill-dev.s3.eu-west-3.amazonaws.com'],
   },
@@ -11,4 +13,22 @@ module.exports = {
       }
     ]
   },
+  sentry: {
+    hideSourceMaps: true,
+  }
 }
+
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
