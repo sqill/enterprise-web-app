75e0bedc74146c9cefbcd50619ac9f2c41adc196
Andre Goncalves <andrepcg@gmail.com>
2024-02-24 15:12:10 +0000

    fix sentry

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 // next.config.js
-const { withSentryConfig } = require("@sentry/nextjs");
 
 const moduleExports = {
   images: {
@@ -26,22 +25,6 @@ const moduleExports = {
     hideSourceMaps: true,
   }
 }
-
-
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
-
 
 // Injected content via Sentry wizard below
 
